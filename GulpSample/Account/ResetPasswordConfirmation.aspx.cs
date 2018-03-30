0f9eb3ca4c092fee9a7ce572d43fa5deb986e37d
Venkata Ravula <vr.xyzgeo@gmail.com>
2018-03-30 07:41:57 -0700

    Initial commit

---
+++ GulpSample/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GulpSample.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GulpSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GulpSample.Startup))]
+namespace GulpSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
